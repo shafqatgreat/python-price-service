2db9078a5fdc915ef6ea63fbb3cba4a32e21e61c
Shafqat Altaf <shafqat@yahoo.com>
2026-02-03 12:55:54 +0500

    Initial FastAPI deployment

---
+++ api/index.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/api/health")
+async def health_check():
+    return {
+        "status": "UP",
+        "service": "price-service-python",
+        "message": "Basic FastAPI is running on Vercel"
+    }
+
+@app.get("/api/hello")
+async def hello():
+    return {"message": "Hello from Python!"}
